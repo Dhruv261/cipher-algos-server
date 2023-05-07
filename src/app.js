50f3b946cabf3dfd776873b91ba979459b93d970
Dhruv <55016342+Dhruv261@users.noreply.github.com>
2023-05-07 22:43:04 +0530

    serverless try

--- src/app.js
+++ src/app.js
@@ -7,13 +7,15 @@ require('./database/mongoose');
 app.use(express.json());
 const userRouter = require('./routes/user');
 const playlistRouter = require('./routes/PassWork');
+const ServerlessHttp = require('serverless-http');
 app.use(express.static('public'));
 
 app.use(userRouter);
 app.use(playlistRouter);
 
+// const port = process.env.PORT || 3001;
+// app.listen(port, () => {
+//   console.log(`Server started at ${port}`);
+// });
 
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`Server started at ${port}`);
-});
+module.exports.handler = ServerlessHttp(app);
